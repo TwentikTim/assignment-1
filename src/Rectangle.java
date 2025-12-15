aab60e1d31b65fe79a61fa07b5a528fec42ccb36
TwentikTim <timurg2qwer123@gmail.com>
2025-12-16 00:46:18 +0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,61 +1,63 @@
 public class Rectangle {
 
-    
     private double width;
     private double height;
-    private static int idGen = 0;
     private int id;
+    private static int idGen = 0;
 
-
+    
     public Rectangle() {
         this.id = idGen++;
         this.width = 1.0;
         this.height = 1.0;
     }
 
-
+    
     public Rectangle(double width, double height) {
         this();
         setWidth(width);
         setHeight(height);
     }
 
-
-    public int getId() {
-        return id;
-    }
-
-
+    
     public double getWidth() {
         return width;
     }
 
     public void setWidth(double width) {
         if (width <= 0) {
-            throw new IllegalArgumentException("Width must be > 0");
+            throw new IllegalArgumentException("Width must be greater than 0");
         }
         this.width = width;
     }
 
-
+    
     public double getHeight() {
         return height;
     }
 
     public void setHeight(double height) {
         if (height <= 0) {
-            throw new IllegalArgumentException("Height must be > 0");
+            throw new IllegalArgumentException("Height must be greater than 0");
         }
         this.height = height;
     }
 
-
+    
     public double area() {
         return width * height;
     }
 
-
+    
     public double perimeter() {
         return 2 * (width + height);
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Rectangle{id=" + id +
+               ", width=" + width +
+               ", height=" + height + "}";
     }
 }
